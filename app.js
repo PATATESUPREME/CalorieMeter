6ea11b46a98d014f95f4978351eaa9a24cbabda6
kde@emakina.fr <kde@emakina.fr>
2017-03-06 11:38:03 +0100

    [Documentation] Add a little bit of doc in app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 /**
  * Product class
  *
- * @param id
- * @param name
- * @param calories
- * @param salt
- * @param quantity
+ * @param {string} id       Product's Id
+ * @param {string} name     Product's name
+ * @param {number} calories Product's calories
+ * @param {number} salt     Product's salt
+ * @param {int}    quantity Product's quantity
  * @constructor
  */
 function Product(id, name, calories, salt, quantity = 1) {
+    /** Self variable overrides bad behavior of this */
     let self = this;
 
     self.id = id;
@@ -21,12 +22,13 @@ function Product(id, name, calories, salt, quantity = 1) {
 /**
  * Profile class
  *
- * @param name
- * @param calories
- * @param salt
+ * @param {string} name     Profile's name
+ * @param {number} calories Profile's calories recommended
+ * @param {number} salt     Profile's salt recommended
  * @constructor
  */
 function Profile(name, calories, salt) {
+    /** Self variable overrides bad behavior of this */
     let self = this;
 
     self.name = name;
@@ -37,30 +39,43 @@ function Profile(name, calories, salt) {
 /**
  * Paginate class
  *
- * @param page
- * @param nbPages
- * @param handler
+ * @param {int}      page    Paginate's current page
+ * @param {int}      nbPages Paginate's total pages
+ * @param {function} handler Paginate's handler when changing page
  * @constructor
  */
 function Paginate(page, nbPages, handler) {
+    /** Self variable overrides bad behavior of this */
     let self = this;
 
     self.page = ko.observable(page);
     self.nbPages = ko.observable(nbPages);
     self.handler = handler;
 
+    /**
+     * Function to go to the first page
+     */
     self.first = function () {
         self.page(1);
         self.handler();
     };
+    /**
+     * Function to go to the previous page
+     */
     self.previous = function () {
         self.page(self.page() - 1);
         self.handler();
     };
+    /**
+     * Function to go to the next page
+     */
     self.next = function () {
         self.page(self.page() + 1);
         self.handler();
     };
+    /**
+     * Function to go to the last page
+     */
     self.last = function () {
         self.page(self.nbPages());
         self.handler();
@@ -73,14 +88,23 @@ function Paginate(page, nbPages, handler) {
  * @constructor
  */
 function AppModelView() {
+    /** Self variable overrides bad behavior of this */
     let self = this;
 
-    /** Search variables */
+    /**
+     * Search
+     */
+    /** Search's input for the product searched */
     self.productSearched = ko.observable();
+    /** Search's input for the minimum calories wanted */
     self.minCalSearched = ko.observable(0);
+    /** Search's input for the maximum calories wanted */
     self.maxCalSearched = ko.observable(50000);
-    self.productFounded = ko.observable(0);
+    /** Number of products found */
+    self.productFound = ko.observable(0);
+    /** Product's list */
     self.productList = ko.observableArray();
+    /** Product's paginate */
     self.productPaginate = new Paginate(1, 0, function() {
         return self.launchSearch();
     });
@@ -89,24 +113,32 @@ function AppModelView() {
      * Function launching the research of a list of products
      */
     self.launchSearch = function () {
+        /** Verify if its a new search */
         if ("search_button" == event.target.id ||
             "product_searched" == event.target.id ||
             "min_cal_searched" == event.target.id ||
             "max_cal_searched" == event.target.id) {
             self.productPaginate.page(1);
         }
+        /** Number of elements in the table (Can be up to max 20 with nutritionix) */
         let interval = 5;
+        /** Calculate the limit and offset of the request */
         let end = interval * self.productPaginate.page();
         let start = end - interval;
+        /** Which fields we want to get in response (* to get everything) */
         let fields = "*";
+        /** My appId and appKey to be able to access nutritionix */
         let appId = "d62702c9";
         let appKey = "3b33a35fc17e370e050895ced60c1798";
+        /** The product searched in the search's input of the product */
         let product = self.productSearched();
 
+        /** Verify if there is a product else we try to find all of them with * */
         if (null == product) {
             product = '*';
         }
 
+        /** A jQuery http request to get the products from nutritionix */
         $.getJSON("https://api.nutritionix.com/v1_1/search/" + product +
             "?results=" + start + "%3A" + end +
             "&cal_min=" + self.minCalSearched() +
@@ -115,19 +147,29 @@ function AppModelView() {
             "&appId=" + appId +
             "&appKey=" + appKey,
             function(data) {
-                console.log(data);
+                /** Initialise a minimum number of pages */
                 let nbPages = 1;
 
-                self.productFounded(data.total_hits);
-                if (10000 < data.total_hits) {
+                /** Set the total of products found */
+                self.productFound(data.total_hits);
+
+                /** Verify if the total of products found is over 10000 because it's the limit of nutritionix's request */
+                if (10000 < self.productFound()) {
+                    /** Set the number of page to maximum */
                     nbPages = parseInt(10000 / interval) + 1;
                 } else {
-                    nbPages = parseInt(data.total_hits / interval) + 1;
+                    /** Set the number of page accordingly to the number of products found */
+                    nbPages = parseInt(self.productFound() / interval) + 1;
                 }
+                /** Set the number of page of the pagination */
                 self.productPaginate.nbPages(nbPages);
 
+                /** Remove all existing product in the product list */
                 self.productList.removeAll();
+
+                /** Add the new products to the list */
                 data.hits.forEach(function (product) {
+                    /** Push a new Product object to the product list */
                     self.productList.push(
                         new Product(
                             product.fields.item_id,
@@ -140,20 +182,31 @@ function AppModelView() {
         ;
     };
 
-    /** Basket variables */
+    /**
+     * Basket
+     */
+    /** Basket's list */
     self.basketList = ko.observableArray();
+    /** Basket's list start index */
     self.basketStartIndex = ko.observable(0);
+    /** Basket's number of elements in table */
     self.basketInterval = ko.observable(5);
+    /** Basket's paginate */
     self.basketPaginate = new Paginate(1, 0, function() {
         return self.changePageBasket();
     });
+    /** Basket's list sliced, ready to be displayed */
     self.basketListSliced = ko.computed(function() {
         self.basketPaginate.nbPages(parseInt((self.basketList().length - 1) / self.basketInterval()) + 1);
 
         return self.basketList.slice(self.basketStartIndex(), self.basketStartIndex() + self.basketInterval());
     });
 
+    /**
+     * Function that handles the page changes
+     */
     self.changePageBasket = function () {
+        /** Set the start index of the basket */
         self.basketStartIndex(self.basketPaginate.page() * self.basketInterval() - self.basketInterval());
     };
 
@@ -163,10 +216,15 @@ function AppModelView() {
      * @param product
      */
     self.addProductToBasket = function (product) {
+        /** Get the index of the product */
         let index = self.basketList.indexOf(product);
+
+        /** Check if it already exists */
         if(index > -1) {
+            /** Augment the quantity of the existing product */
             self.basketList()[index].quantity(self.basketList()[index].quantity() + 1);
         } else {
+            /** Add the product to the basket list */
             self.basketList.push(product);
         }
     };
@@ -177,33 +235,25 @@ function AppModelView() {
      * @param product
      */
     self.removeProductOfBasket = function (product) {
+        /** Get the index of the product */
         let index = self.basketList.indexOf(product);
+        /** Get the quantity of the product */
         let quantity = self.basketList()[index].quantity();
+
+        /** Check if there is more than one product in the basket list */
         if(quantity > 1) {
+            /** Decrease the quantity of the product */
             self.basketList()[index].quantity(self.basketList()[index].quantity() - 1);
         } else {
+            /** Remove the product of the list */
             self.basketList.remove(product);
         }
     };
 
-    /** Profile variables */
-    self.selectedProfile = ko.observable();
-    self.caloriesProgress = ko.observable(0);
-    self.caloriesStatus = ko.computed(function() {
-        if (self.caloriesProgress() > 100) {
-            return "bg-danger";
-        } else {
-            return "bg-success";
-        }
-    });
-    self.saltProgress = ko.observable(0);
-    self.saltStatus = ko.computed(function() {
-        if (self.saltProgress() > 100) {
-            return "bg-danger";
-        } else {
-            return "bg-success";
-        }
-    });
+    /**
+     * Profile variables
+     */
+    /** Array of available profiles */
     self.availableProfiles = ko.observableArray([
         new Profile("Child", 1600, 5),
         new Profile("Teenage boy", 2900, 5),
@@ -216,39 +266,83 @@ function AppModelView() {
         new Profile("Old man", 2000, 5),
         new Profile("Old woman", 1800, 5),
     ]);
-    self.saltQuantity = ko.computed(function() {
-        let total = 0;
-        let maxSalt = 5;
-
-        ko.utils.arrayForEach(self.basketList(), function(product) {
-            total += product.salt * product.quantity();
-        });
-
-        if (undefined != self.selectedProfile()) {
-            maxSalt = self.selectedProfile().salt
+    /** Current profile selected */
+    self.selectedProfile = ko.observable();
+    /** Status of the calories of the current profile */
+    self.caloriesProgress = ko.observable(0);
+    /** Set the color according to the calories' status */
+    self.caloriesStatus = ko.computed(function() {
+        /** Check there is too much calories */
+        if (self.caloriesProgress() > 100) {
+            /** Announce to much calories */
+            return "bg-danger";
+        } else {
+            /** The status is ok */
+            return "bg-success";
         }
-
-        self.saltProgress(total * 100 / maxSalt);
-
-        return total + " / " + maxSalt + "g";
     });
+    /** Status of the salt of the current profile */
+    self.saltProgress = ko.observable(0);
+    /** Set the color according to the salt's status */
+    self.saltStatus = ko.computed(function() {
+        /** Check there is too much salt */
+        if (self.saltProgress() > 100) {
+            /** Announce to much salt */
+            return "bg-danger";
+        } else {
+            /** The status is ok */
+            return "bg-success";
+        }
+    });
+    /** Compute the quantity of calories in the basket */
     self.caloriesQuantity = ko.computed(function() {
-        let total = 0;
+        /** Initialise the quantity of calories */
+        let quantity = 0;
+        /** Initialise the maximum calories according to Wikipedia */
         let maxCalories = 2500;
 
+        /** Calculate the quantity of calories in the basket */
         ko.utils.arrayForEach(self.basketList(), function(product) {
-            total += product.calories * product.quantity();
+            quantity += product.calories * product.quantity();
         });
 
+        /** Check if there is a profile selected */
         if (undefined != self.selectedProfile()) {
+            /** Set the maximum according to the profile */
             maxCalories = self.selectedProfile().calories
         }
 
-        self.caloriesProgress(total * 100 / maxCalories);
+        /** Set the status of the calories */
+        self.caloriesProgress(quantity * 100 / maxCalories);
 
-        return total + " / " + maxCalories + "kcal";
+        /** Returns the amount of calories in the basket */
+        return quantity + " / " + maxCalories + "kcal";
+    });
+    /** Compute the quantity of salt in the basket */
+    self.saltQuantity = ko.computed(function() {
+        /** Initialise the quantity of salt */
+        let quantity = 0;
+        /** Initialise the maximum calories according to Wikipedia */
+        let maxSalt = 5;
+
+        /** Calculate the quantity of salt in the basket */
+        ko.utils.arrayForEach(self.basketList(), function(product) {
+            quantity += product.salt * product.quantity();
+        });
+
+        /** Check if there is a profile selected */
+        if (undefined != self.selectedProfile()) {
+            /** Set the maximum according to the profile */
+            maxSalt = self.selectedProfile().salt
+        }
+
+        /** Set the status of the salt */
+        self.saltProgress(quantity * 100 / maxSalt);
+
+        /** Returns the amount of salt in the basket */
+        return quantity + " / " + maxSalt + "g";
     });
 }
 
-// Activates knockout.js
+/** Activates knockout.js */
 ko.applyBindings(new AppModelView());
